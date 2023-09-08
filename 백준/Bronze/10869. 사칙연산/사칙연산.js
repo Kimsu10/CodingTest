763de66e3da7ab5b656957f62f8d1f8f1f6a01c7
Kimsu <107941176+Kimsu10@users.noreply.github.com>
2023-09-09 00:29:49 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync("dev/stdin").toString().split(" ");
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(Math.trunc(A/B));
+console.log(A%B);
