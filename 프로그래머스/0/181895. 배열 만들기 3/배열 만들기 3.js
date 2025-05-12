37a0f5881ac44ecf1f830f7953c928d35e205285
Kimsu <107941176+Kimsu10@users.noreply.github.com>
2025-05-13 00:04:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.93 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,10 @@
+function solution(arr, intervals) {
+    
+    let result = [];
+    
+    for(let i = 0; i< intervals.length; i++){
+        result.push(arr.slice(intervals[i][0], intervals[i][1]+1))
+    }
+    
+    return result.flat();
+}
