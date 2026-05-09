e66dcfa0ca67112a9ed8108111ebd3df91390277
Kimsu <107941176+Kimsu10@users.noreply.github.com>
2026-05-10 01:28:06 +0900

    [level 0] Title: 정수 찾기, Time: 1.35 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,19 @@
+// class Solution {
+//     public int solution(int[] num_list, int n) {
+//         for(int i = 0; i < num_list.length; i++){
+//             if(num_list[i] == n){
+//               return 1;  
+//             } 
+//         }
+//         return 0;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        for(int i = 0; i < num_list.length; i++){
-            if(num_list[i] == n){
-              return 1;  
-            } 
-        }
-        return 0;
+        return Arrays.stream(num_list)
+                .anyMatch(num -> num == n) ? 1 : 0;
     }
 }
